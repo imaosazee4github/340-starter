00a4fe2004bf249c4172aa9ce0c48d78459c851b
imaosazee <imaosazee@gmail.com>
2026-04-01 21:12:45 +0100

    fixed bug

--- utilities/index.js
+++ utilities/index.js
@@ -61,31 +61,31 @@ Util.buildClassificationGrid = async function(data){
 }
 
 Util.buildVehicleDetail = async function(vehicle) {
+  const miles = new Intl.NumberFormat("en-US").format(vehicle.inv_miles)
+  const price = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  }).format(vehicle.inv_price)
+
+
   let detail = `
-  <div class="vehicle-detail">
+   <div class="vehicle-detail">
 
     <div class="vehicle-image">
       <img src="${vehicle.inv_image}" 
-           alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
+           alt="${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}">
     </div>
-     <div class="vehicle-info">
+
+    <div class="vehicle-info">
       <h2>${vehicle.inv_year} ${vehicle.inv_make} ${vehicle.inv_model}</h2>
 
-      <p class="price">
-        <strong>Price:</strong> $${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}
-      </p>
+      <p><strong>Price:</strong> ${price}</p>
 
-      <p>
-        <strong>Description:</strong> ${vehicle.inv_description}
-      </p>
+      <p><strong>Mileage:</strong> ${miles} miles</p>
 
-      <p>
-        <strong>Color:</strong> ${vehicle.inv_color}
-      </p>
+      <p><strong>Color:</strong> ${vehicle.inv_color}</p>
 
-      <p>
-        <strong>Mileage:</strong> ${new Intl.NumberFormat('en-US').format(vehicle.inv_miles)} miles
-      </p>
+      <p><strong>Description:</strong> ${vehicle.inv_description}</p>
     </div>
 
   </div>
