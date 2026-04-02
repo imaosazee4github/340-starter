967e6116363f6d891400645bef9191e176a8e665
imaosazee <imaosazee@gmail.com>
2026-04-02 13:34:45 +0100

    localhost_update

--- database/index.js
+++ database/index.js
@@ -34,8 +34,6 @@
 const { Pool } = require("pg")
 require("dotenv").config()
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL)
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5500
 
 
 
