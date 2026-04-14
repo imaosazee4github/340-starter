df55f1ff4241743aab3fc6353b39645743e128c8
imaosazee <imaosazee@gmail.com>
2026-04-14 01:44:19 +0100

    updated-wk-4

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,11 +4,16 @@ const invModel = require("../models/inventory-model")
 const baseController = {}
 
 baseController.buildHome = async function(req, res, next) {
-    try{
-    const nav = await utilities.getNav() 
-} catch(error) {
-next(error)
-}
+    try {
+        const nav = await utilities.getNav()
+
+        res.render("index", {
+            title: "Home",
+            nav
+        })
+    } catch(error) {
+        next(error)
+    }
 }
 
 module.exports = baseController
